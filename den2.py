d82240680141073e46e6f58f70d574ea2b306e15
Yunus Ayyıldız <ayyildizy824@gmail.com>
2025-10-01 10:59:34 +0300

    ders örnek

---
+++ den2.py
@@ -0,0 +1,19 @@
+gün=int(input("gün:"))
+
+if gün == 1:
+    print("pazartesi")
+elif gün ==2:
+    print("salı")
+elif gün ==3:
+    print("çarşamba")
+elif gün ==4:
+    print("perşembe")
+elif gün ==5:
+    print("cuma")
+elif gün ==6:
+    print("cumartesi")
+elif gün ==7:
+    print("pazar")
+else :
+    print("geçersiz değer")
+
