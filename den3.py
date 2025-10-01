cb0d4f241221c69fca2e28964653080387b0f63f
Yunus Ayyıldız <ayyildizy824@gmail.com>
2025-10-01 11:00:07 +0300

    ders

---
+++ den3.py
@@ -0,0 +1,17 @@
+pzts=int(input("pzts:"))
+if pzts==1:
+    print("Pazartesi")
+elif salı ==2:
+    print("salı")
+elif çrm == 3:
+    print("çarşamba")
+elif prş ==4:
+    print("perşembe")
+elif cuma ==5:
+    print("cuma")
+elif cmts ==6:
+    print("cumartesi")
+elif pzr ==7:
+    print("pazar")
+else:
+    print("yanlış değer")
