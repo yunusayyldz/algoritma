5239073b0e4112bd8c4eb6a5492ec03bc235f8cd
Yunus Ayyıldız <ayyildizy824@gmail.com>
2025-10-15 10:52:01 +0300

    ders_08.py

---
+++ ders_08.py
@@ -0,0 +1,10 @@
+while True:
+    tahmin=int(input("tahmin:"))
+    if tahmin ==1925 :
+        print("tebrikler")
+
+        break
+    elif tahmin < 1925 :
+        print("daha büyük")
+    else:
+        print("daha küçük")
