b93562a1d60c3dca9720bd7640a4b97408e304b3
Yunus Ayyıldız <ayyildizy824@gmail.com>
2025-10-22 10:43:44 +0300

    Add function to double input value

---
+++ ders_14.py
@@ -0,0 +1,13 @@
+import math #kütüphane eklemek için
+
+def kat(arg):
+
+    return 2*arg
+
+r=int(input("değer:"))
+
+kat(r) 
+#r yazmamız da sıkıtı yok çünkü parametre giriyoruz
+
+sonuç=kat(r)
+print(sonuç)
