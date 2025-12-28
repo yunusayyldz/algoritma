3794e4b89039e43e9749ef42928834b9fab0b682
Yunus Ayyıldız <ayyildizy824@gmail.com>
2025-12-28 15:43:45 +0300

    Add islem function to calculate modified product

---
+++ final/final01.py
@@ -0,0 +1,7 @@
+def islem(num1,num2):
+    carp = num1 * num2
+    sonuc = carp * 2 / 7
+    return sonuc
+sayi1 = int(input("1. sayıyı giriniz:"))
+sayi2 = int(input("2. sayıyı giriniz:"))
+print("sonuç:",islem(sayi1,sayi2))
