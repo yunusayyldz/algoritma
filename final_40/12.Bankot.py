0a1e9b846fb3a851c9b4fbbe02700f8703d05611
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 15:09:33 +0300

    Update 12.Bankot.py

--- final_40/12.Bankot.py
+++ final_40/12.Bankot.py
@@ -1,23 +1,12 @@
-# 1. Adım: Banknotlarımızı tanımlıyoruz
-# Büyükten küçüğe sıralamak ZORUNLUDUR.
-# En sona 1 ekledik ki bozuk paraları da hesaplayabilsin.
-banknot_listesi = [200, 100, 50, 20, 10, 5, 1]
+banknot = [200, 100, 50, 20, 10, 5, 1]
 
-# 2. Adım: Kullanıcıdan para miktarını alıyoruz
 para = int(input("Lütfen para miktarını giriniz: "))
 
-print(f"\n--- {para} TL için Banknot Dağılımı ---")
+for değer in banknot:
 
-# 3. Adım: Her bir banknotu sırasıyla deniyoruz
-for banknot in banknot_listesi:
-    # Bu banknottan kaç tane verebiliriz? (Tam bölme)
-    adet = para // banknot
-    
-    # Eğer bu banknottan en az 1 tane verebiliyorsak:
+    adet = para // değer
+
     if adet > 0:
-        print(f"{adet} x {banknot} TL")
+        print(adet,"x",değer)
         
-        # Verdiğimiz miktarı ana paradan düşüyoruz (Kalanı buluyoruz)
-        para = para % banknot
-
-# İşlem bittiğinde 'para' değişkeni 0 olur.
+        para = para % değer
