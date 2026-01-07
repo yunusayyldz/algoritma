478843f788e36568c91b44dee69c1d19ed4abd8b
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:28:39 +0300

    Update 16.çakma_round.py

--- final_40/16.çakma_round.py
+++ final_40/16.çakma_round.py
@@ -1,16 +1,13 @@
-#round = yuvarlama
 def benim_yuvarlama(sayi):
-    # Eğer sayı pozitifse 0.5 ekleyip tam kısmını alırız
+
     if sayi >= 0:
         sonuc = int(sayi + 0.5)
-    # Eğer sayı negatifse 0.5 çıkarıp tam kısmını alırız
+
     else:
         sonuc = int(sayi - 0.5)
         
     return sonuc
 
-# --- Test Edelim ---
-# Kullanıcıdan ondalıklı (float) bir sayı alıyoruz
 girilen_sayi = float(input("Yuvarlanacak sayıyı giriniz: "))
 
 yeni_hali = benim_yuvarlama(girilen_sayi)
