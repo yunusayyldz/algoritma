90e59d67d8c9a2d83b11f62091dadc8883032512
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:21:49 +0300

    Update 24.VirgüldenS_3Basamak.py

--- final_40/24.VirgüldenS_3Basamak.py
+++ final_40/24.VirgüldenS_3Basamak.py
@@ -1,16 +1,7 @@
-# 1. Adım: Kullanıcıdan a sayısını alıyoruz
-a = int(input("Bir sayı (a) giriniz: "))
+sayi = float(input("sayıyı giriniz"))
 
-# 2. Adım: Bölme işleminde 0 hatası olmasın diye kontrol ediyoruz
-if a == 0:
-    print("Hata: Bir sayı 0'a bölünemez!")
+if sayi == 0 :
+    print("0 bölünmez")
 else:
-    # --- Matematiksel Hile ---
-    # 1/a işlemini yap, virgülü 3 basamak sağa kaydır (1000 ile çarp)
-    # Sonra int() ile küsuratı tamamen at.
-    deger = (1 / a) * 1000
-    
-    # Sadece tam kısmı alıyoruz
-    ilk_uc_basamak = int(deger)
-    
-    print(f"1/{a} sayısının virgülden sonraki ilk 3 basamağı: {ilk_uc_basamak}")
+    sonuc = 1 / sayi
+    print(f"sonuc : {sonuc:.3f}")
