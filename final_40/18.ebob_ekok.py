5144ddcb7ee7df33b4caa8e8affc07c6937f0741
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:13:59 +0300

    Update 18.ebob_ekok.py

--- final_40/18.ebob_ekok.py
+++ final_40/18.ebob_ekok.py
@@ -1,28 +1,15 @@
-def ebob_ekok_hesapla(sayi1, sayi2):
+def ebob_ekok(s1, s2):
+    a, b = s1, s2
 
-    if sayi1 < sayi2:
-        kucuk_sayi = sayi1
-    else:
-        kucuk_sayi = sayi2
+    while b:
+        a, b = b, a % b  
     
-    ebob = 1 
-
-    for i in range(kucuk_sayi, 0, -1):
-
-        if (sayi1 % i == 0) and (sayi2 % i == 0):
-            ebob = i
-            break 
-            
-    ekok = (sayi1 * sayi2) // ebob
-    
+    ebob = a
+    ekok = (s1 * s2) // ebob
     return ebob, ekok
+        
+s1 = int(input("1. Sayı: "))
+s2 = int(input("2. Sayı: "))
 
-
-s1 = int(input("Birinci sayıyı giriniz: "))
-s2 = int(input("İkinci sayıyı giriniz: "))
-
-sonuc_ebob, sonuc_ekok = ebob_ekok_hesapla(s1, s2)
-
-print(f"--- Sonuçlar ---")
-print("EBOB ",sonuc_ebob)
-print("EKOK ",sonuc_ekok)
+ebob, ekok = ebob_ekok(s1, s2)
+print(f"EBOB: {ebob}\nEKOK: {ekok}")
