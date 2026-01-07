1ac7baae1ee34ac78ed0e9f309c3f57cad7d3029
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 14:42:34 +0300

    Update 02.harf_rakam_sim.py

--- final_40/02.harf_rakam_sim.py
+++ final_40/02.harf_rakam_sim.py
@@ -1,16 +1,10 @@
-# 1. Adım: Kullanıcıdan karakteri alıyoruz
 veri = input("Lütfen klavyeden bir tuşa basınız (Tek karakter): ")
 
-# 2. Adım: Karakterin türünü kontrol ediyoruz
-
-# .isalpha() -> "Alphabet" kelimesinden gelir. "Bu bir harf mi?" diye sorar.
 if veri.isalpha():
     print("Girdiğiniz karakter bir HARF'tir.")
 
-# .isdigit() -> "Digit" kelimesinden gelir. "Bu bir rakam mı?" diye sorar.
 elif veri.isdigit():
     print("Girdiğiniz karakter bir RAKAM'dır.")
 
-# Yukarıdakilerin hiçbiri değilse, geriye simge kalır.
 else:
     print("Girdiğiniz karakter bir SİMGE'dir.")
