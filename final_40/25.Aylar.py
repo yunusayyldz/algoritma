f54163e9c3d05b80b97606481bcc03a9df0333e8
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:32:37 +0300

    Update 25.Aylar.py

--- final_40/25.Aylar.py
+++ final_40/25.Aylar.py
@@ -1,20 +1,13 @@
-# 1. Adım: Ay isimlerini hafızaya alıyoruz
-# Listeyi oluştururken başına boş tırnak "" koyduk.
-# Böylece aylar[1] dediğimizde direkt "Ocak" gelecek.
+
 aylar = ["", "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", 
          "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
 
-# 2. Adım: Kullanıcıdan numarayı alıyoruz
 ay_no = int(input("Lütfen ay numarasını (1-12) giriniz: "))
 
-# 3. Adım: Geçerli bir sayı mı diye kontrol ediyoruz
 if 1 <= ay_no <= 12:
-    
-    # Ayın ismini listeden çekiyoruz
+
     ay_ismi = aylar[ay_no]
-    
-    # Mevsimi belirliyoruz
-    # "in" komutu "bu listenin içinde mi?" diye bakar.
+
     if ay_no in [12, 1, 2]:
         mevsim = "Kış"
     elif ay_no in [3, 4, 5]:
@@ -24,9 +17,8 @@ if 1 <= ay_no <= 12:
     else:
         mevsim = "Sonbahar"
     
-    # 4. Adım: Sonucu istenen formatta yazdırıyoruz
     print(f"{ay_no} -> {ay_ismi} -> {mevsim}")
 
 else:
-    # Hatalı giriş yapılırsa
+
     print("Hata: Lütfen 1 ile 12 arasında bir sayı giriniz!")
