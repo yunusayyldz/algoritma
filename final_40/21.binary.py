38c8e31a58e91ef24790bf619f6e7f9f4b4a9783
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:30:36 +0300

    Update 21.binary.py

--- final_40/21.binary.py
+++ final_40/21.binary.py
@@ -1,24 +1,19 @@
-#2 lik sayı sistemine göre
 def binarye_cevir(sayi):
-    # Özel Durum: Sayı 0 ise direkt "0" döndür
+    
     if sayi == 0:
         return "0"
     
-    # Sonucu biriktireceğimiz boş bir metin (string)
     binary_karsilik = ""
     
     while sayi > 0:
-        kalan = sayi % 2      # 2'ye bölümünden kalanı bul (0 veya 1)
+        kalan = sayi % 2     
         
-        # DİKKAT: Kalanı mevcut sonucun BAŞINA ekliyoruz.
-        # Çünkü binary sistemde son kalan en başa yazılır.
         binary_karsilik = str(kalan) + binary_karsilik
         
-        sayi = sayi // 2      # Sayıyı ikiye bölüp küçültüyoruz
+        sayi = sayi // 2   
         
     return binary_karsilik
 
-# --- Test Edelim ---
 girilen_sayi = int(input("Bir sayı giriniz: "))
 
 sonuc = binarye_cevir(girilen_sayi)
