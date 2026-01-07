b9892055c36ac65dc70b6a6ef3aaae2305981dea
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:38:35 +0300

    Update 36.tekrar_eleman.py

--- final_40/36.tekrar_eleman.py
+++ final_40/36.tekrar_eleman.py
@@ -3,18 +3,18 @@ def tekrar_edenleri_bul(liste):
     tekrar_edenler = set()
     
     for eleman in liste:
-        # Eğer bu elemanı daha önce gördüysek, bu bir kopyadır
+
         if eleman in gorulenler:
             tekrar_edenler.add(eleman)
         else:
-            # İlk kez görüyorsak, görülenler listesine ekle
+
             gorulenler.add(eleman)
             
     return list(tekrar_edenler)
 
-# --- Ana Program ---
+
 veri = input("Elemanları aralarında boşluk bırakarak giriniz: ")
-orijinal_liste = veri.split() # Girişi listeye çevir
+orijinal_liste = veri.split() 
 
 sonuc = tekrar_edenleri_bul(orijinal_liste)
 
