aa4a2eee4bf9536937517eebf772b1a60415c068
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 18:38:15 +0300

    Update 35.kes_birleşim.py

--- final_40/35.kes_birleşim.py
+++ final_40/35.kes_birleşim.py
@@ -1,22 +1,14 @@
 def kume_islemleri(liste_a, liste_b):
-    # 1. Adım: Listeleri kümeye (set) çeviriyoruz
-    # Böylece hem kopyalar silinir hem de matematiksel işlem yapılabilir.
+
     kume_a = set(liste_a)
     kume_b = set(liste_b)
     
-    # 2. Adım: Kesişim (Intersection) -> İkisinde de olanlar
-    # "&" operatörü veya .intersection() metodu kullanılır.
     kesisim = kume_a & kume_b
-    
-    # 3. Adım: Birleşim (Union) -> Hepsi (Tekrarsız)
-    # "|" operatörü veya .union() metodu kullanılır.
+
     birlesim = kume_a | kume_b
-    
-    # Sonuçları tekrar listeye çevirip döndürüyoruz
+
     return list(kesisim), list(birlesim)
 
-# --- Test Edelim ---
-# Örnek Listeler
 l1 = [1, 2, 3, 4, 5]
 l2 = [4, 5, 6, 7, 8]
 
