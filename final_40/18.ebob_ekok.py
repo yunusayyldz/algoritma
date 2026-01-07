35d354f04fd46dcfe70f670def716991a1bfc4ec
Yunus Ayyıldız <ayyildizy824@gmail.com>
2026-01-07 15:16:57 +0300

    Update 18.ebob_ekok.py

--- final_40/18.ebob_ekok.py
+++ final_40/18.ebob_ekok.py
@@ -1,35 +1,28 @@
 def ebob_ekok_hesapla(sayi1, sayi2):
-    # 1. ADIM: EBOB BULMA
-    # Önce küçük sayıyı bulalım ki döngü kısa sürsün
+
     if sayi1 < sayi2:
         kucuk_sayi = sayi1
     else:
         kucuk_sayi = sayi2
     
-    ebob = 1 # Varsayılan değer
-    
-    # Küçük sayıdan başlayıp 1'e doğru geriye sayıyoruz
+    ebob = 1 
+
     for i in range(kucuk_sayi, 0, -1):
-        # Eğer 'i' hem sayı1'i hem sayı2'yi tam bölüyorsa EBOB odur.
+
         if (sayi1 % i == 0) and (sayi2 % i == 0):
             ebob = i
-            break # En büyüğü bulduk, artık döngüyü kırıp çıkabiliriz.
+            break 
             
-    # 2. ADIM: EKOK BULMA (Matematiksel Formül)
-    # // kullanarak tam sayı sonucu istiyoruz (virgül olmasın diye)
     ekok = (sayi1 * sayi2) // ebob
     
-    # İki değeri birden paketleyip gönderiyoruz
     return ebob, ekok
 
-# --- Ana Program ---
 
 s1 = int(input("Birinci sayıyı giriniz: "))
 s2 = int(input("İkinci sayıyı giriniz: "))
 
-# Fonksiyondan dönen iki değeri, virgülle ayırarak iki değişkene alıyoruz
 sonuc_ebob, sonuc_ekok = ebob_ekok_hesapla(s1, s2)
 
 print(f"--- Sonuçlar ---")
-print(f"EBOB ({s1}, {s2}) = {sonuc_ebob}")
-print(f"EKOK ({s1}, {s2}) = {sonuc_ekok}")
+print("EBOB ",sonuc_ebob)
+print("EKOK ",sonuc_ekok)
